90c55617c3dc71946c6d9ccd637429884edfd6c2
JulianAballay <juliancabj1210@gmail.com>
2023-11-07 13:15:25 -0300

    Agrego logica ejercicio 1 arrays y estilo css

---
+++ js/ejercicioArrayUno.js
@@ -0,0 +1,7 @@
+const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+document.write('<h2>Lista de Meses</h2>');
+document.write('<ul id="listaMeses">');
+for(let i = 0; i < meses.length; i++){
+    document.write(`<li>${meses[i]}</li>`);
+}
+document.write('</ul>');
